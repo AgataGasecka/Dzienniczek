8854f57ab71dccc54c43b54b64e6355d55712ac2
Zuzanna Salkowska <zuzanna.salkowska@nexpertis.pl>
2018-10-09 22:03:54 +0200

    settings layout

---
+++ DzienniczekPacjenta/app/src/main/java/com/example/agata/dzienniczekpacjenta/Home.java
@@ -0,0 +1,20 @@
+package com.example.agata.dzienniczekpacjenta;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void goToSettings(View view){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+}
