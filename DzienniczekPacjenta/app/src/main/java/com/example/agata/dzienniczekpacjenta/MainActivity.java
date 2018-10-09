ccc81d8accd2a7185c68b6d51808c050dd10127c
AgataGasecka <gaseckaagata@gmail.com>
2018-10-09 16:50:03 +0200

    zmiany Kasi

--- DzienniczekPacjenta/app/src/main/java/com/example/agata/dzienniczekpacjenta/MainActivity.java
+++ DzienniczekPacjenta/app/src/main/java/com/example/agata/dzienniczekpacjenta/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.agata.dzienniczekpacjenta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+     public void createAnAccount(View view){
+         Intent intent = new Intent(this, NewAccount.class);
+         startActivity(intent);
+     }
+
+     public void singIn(View view){
+         Intent intent = new Intent(this, SingIn.class);
+         startActivity(intent);
+     }
 }
